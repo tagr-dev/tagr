37b89c0a24a2d68ae3fb498552603bdf3eb5f638
Eric Lee <ericjlee@hotmail.com>
2020-08-29 20:13:29 -0400

    fix string formatting and get s3 pushing to work

--- tagr/storage/aws.py
+++ tagr/storage/aws.py
@@ -8,19 +8,17 @@ csv_buffer = StringIO()
 class Aws:
     def dump_csv(self, df, proj, exp, tag, i):
         df.to_csv(csv_buffer)
-        #df.to_csv("s3://{}/{}/{}/{}.csv".format(proj, exp, tag, i), index=False)
-        s3.Object(proj, "s3://{}/{}/{}/{}.csv".format(proj, exp, tag, i)).put(
+        s3.Object(proj, "{}/{}/{}.csv".format(exp, tag, i)).put(
             Body=csv_buffer.getvalue()
         )
     
     def dump_json(self, df_metadata, proj, exp, tag):
-        s3_object = s3.Object(proj, "{}/{}/df_summary.json".format(exp, tag))
-        s3_object.put(
+        s3.Object(proj, "{}/{}/df_summary.json".format(exp, tag)).put(
             Body=(bytes(json.dumps(df_metadata, cls=NpEncoder).encode("UTF-8"))),
             ContentType="application/json",
         )
 
     def dump_pickle(self, pickle_object, proj, exp, tag, i):
-        s3.Object(proj, "{}/{}/{}/{}.pkl".format(proj, exp, tag, i)).put(
-            Body=pickle_byte_obj
+        s3.Object(proj, "{}/{}/{}.pkl".format(exp, tag, i)).put(
+            Body=pickle_object
         )
