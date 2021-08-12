4210e487fef785e8104454112d8148d4a700e9af
sunriselong <sunrise.long@yahoo.ca>
2021-08-12 13:48:09 -0400

    super the init

--- test/unit/test_tagging.py
+++ test/unit/test_tagging.py
@@ -1,12 +1,14 @@
 import unittest
 
-from tagr.tagging.artifacts import Tagr
+from tagr.tagging import Tagr
 
 class TaggingTest(unittest.TestCase):
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.tag = Tagr()
+        super().__init__(*args, **kwargs)
 
     def test_artfact_is_returned(self):
         test_artifact = 'foo'
         res = self.tag.save(test_artifact, 'str')
         self.assertEqual(res, 'foo')
+        
