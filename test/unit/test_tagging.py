68a93bc6f7c122122d2c98a973f5e73009c85f71
sunriselong <sunrise.long@yahoo.ca>
2021-08-12 15:57:02 -0400

    write some tests

--- test/unit/test_tagging.py
+++ test/unit/test_tagging.py
@@ -1,6 +1,8 @@
 import unittest
+import pandas as pd
 
 from tagr.tagging import Tagr
+
 
 class TaggingTest(unittest.TestCase):
     def __init__(self, *args, **kwargs):
@@ -8,7 +10,41 @@ class TaggingTest(unittest.TestCase):
         super().__init__(*args, **kwargs)
 
     def test_artfact_is_returned(self):
-        test_artifact = 'foo'
-        res = self.tag.save(test_artifact, 'str')
-        self.assertEqual(res, 'foo')
-        
+        test_artifact = "foo"
+        res = self.tag.save(test_artifact, "str")
+        self.assertEqual(res, "foo")
+
+    def test_save_with_no_dtype(self):
+        test_artifact = "foo"
+        self.assertRaises(
+            ValueError, lambda: self.tag.save(test_artifact, "unrecognized_obj")
+        )
+
+    def test_obj_name_to_dtype_conversion(self):
+        data = [{"a": 1, "b": 2, "c": 3}, {"a": 10, "b": 20, "c": 30}]
+        self.tag.save(data, "X_train")
+        self.assertEqual(self.tag.queue['X_train'].dtype, 'dataframe')
+
+        # test aliasing
+        test_int = 2
+        self.tag.save(test_int, 'num', 'int')
+        self.assertEqual(self.tag.queue['num'].dtype, 'primitive')
+
+    def test_ignore_dupes(self):
+        self.tag = Tagr()
+        self.tag.save(2, 'num', 'int')
+        self.tag.save(2, 'num', 'int')
+        self.assertEqual(len(self.tag.queue), 1)
+
+    def test_summary(self):
+        data = [{'obj_name': 'num1', 'val': 2.0, 'dtype': 'primitive'},
+        {'obj_name': 'str1', 'val': 'foo', 'dtype': 'primitive'}]
+        expected_df = pd.DataFrame(data)
+
+        self.tag = Tagr()
+        self.tag.save(2, 'num1', 'int')
+        self.tag.save('foo', 'str1', 'float')
+        summary = self.tag.summary()
+        pd.assert_frame_equal(summary, expected_df)
+
+    
