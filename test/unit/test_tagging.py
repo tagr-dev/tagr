978eae0071feef9d3b10050593e05d9235ead42a
sunriselong <sunrise.long@yahoo.ca>
2021-08-13 11:21:33 -0400

    add boto and cleanup tagging tests

--- test/unit/test_tagging.py
+++ test/unit/test_tagging.py
@@ -1,7 +1,7 @@
 import unittest
 import pandas as pd
 
-from tagr.tagging import Tagr
+from tagr.tagging import Tagr, artifacts
 
 
 class TaggingTest(unittest.TestCase):
@@ -23,28 +23,31 @@ class TaggingTest(unittest.TestCase):
     def test_obj_name_to_dtype_conversion(self):
         data = [{"a": 1, "b": 2, "c": 3}, {"a": 10, "b": 20, "c": 30}]
         self.tag.save(data, "X_train")
-        self.assertEqual(self.tag.queue['X_train'].dtype, 'dataframe')
+        df_dtype = self.tag.ret_queue()["X_train"].dtype
+        self.assertEqual(df_dtype, "dataframe")
 
         # test aliasing
         test_int = 2
-        self.tag.save(test_int, 'num', 'int')
-        self.assertEqual(self.tag.queue['num'].dtype, 'primitive')
+        self.tag.save(test_int, "num", "int")
+        int_dtype = self.tag.ret_queue()["num"].dtype
+        self.assertEqual(int_dtype, "primitive")
 
     def test_ignore_dupes(self):
         self.tag = Tagr()
-        self.tag.save(2, 'num', 'int')
-        self.tag.save(2, 'num', 'int')
-        self.assertEqual(len(self.tag.queue), 1)
+        self.tag.save(2, "num", "int")
+        self.tag.save(2, "num", "int")
+        queue_length = len(self.tag.ret_queue())
+        self.assertEqual(queue_length, 1)
 
     def test_summary(self):
-        data = [{'obj_name': 'num1', 'val': 2.0, 'dtype': 'primitive'},
-        {'obj_name': 'str1', 'val': 'foo', 'dtype': 'primitive'}]
+        data = [
+            {"obj_name": "num1", "val": 2.0, "dtype": "primitive"},
+            {"obj_name": "str1", "val": "foo", "dtype": "primitive"},
+        ]
         expected_df = pd.DataFrame(data)
 
         self.tag = Tagr()
-        self.tag.save(2, 'num1', 'int')
-        self.tag.save('foo', 'str1', 'float')
+        self.tag.save(2, "num1", "int")
+        self.tag.save("foo", "str1", "float")
         summary = self.tag.summary()
         pd._testing.assert_frame_equal(summary, expected_df)
-
-    
