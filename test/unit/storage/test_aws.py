6b970a107e900a84240379bf542470f5617a4055
sunriselong <sunrise.long@yahoo.ca>
2021-08-18 15:13:20 -0400

    list table and get get object test

--- test/unit/storage/test_aws.py
+++ test/unit/storage/test_aws.py
@@ -129,3 +129,51 @@ class AwsHelperTest(unittest.TestCase):
         )
         keys_list = [key for key in matching_keys_res]
         self.assertEqual(expected_key, keys_list)
+
+    def test_get_list_of_tables(self):
+        expected_res = [
+            "{}/{}/unit_test_tag/text1.txt".format(PROJ, EXPERIMENT),
+            "{}/{}/unit_test_tag/text2.txt".format(PROJ, EXPERIMENT),
+        ]
+
+        aws_helper = AwsHelper()
+        conn = self.create_connection()
+
+        conn.Object(PROJ, "{}/{}/text1.txt".format(EXPERIMENT, TAG)).put(
+            Body="content_of_text1"
+        )
+        conn.Object(PROJ, "{}/{}/text2.txt".format(EXPERIMENT, TAG)).put(
+            Body="content_of_text2"
+        )
+
+        list_of_tables = aws_helper.get_list_of_tables(
+            bucket=PROJ, object_path="{}/{}".format(EXPERIMENT, TAG)
+        )
+        self.assertEqual(expected_res, list_of_tables)
+
+    def test_get_object(self):
+        expected_df = DF
+        expected_dict = {"test_key": "test_val"}
+
+        storage_provider = Aws()
+        aws_helper = AwsHelper()
+
+        storage_provider.dump_csv(
+            df=DF, proj=PROJ, experiment=EXPERIMENT, tag=TAG, filename="test_data"
+        )
+        df_content = aws_helper.get_object(
+            bucket=PROJ, object_path="{}/{}/test_data.csv".format(EXPERIMENT, TAG)
+        )
+        pd._testing.assert_frame_equal(expected_df, df_content)
+
+        storage_provider.dump_pickle(
+            model=expected_dict,
+            proj=PROJ,
+            experiment=EXPERIMENT,
+            tag=TAG,
+            filename="test_dict",
+        )
+        pickle_content = aws_helper.get_object(
+            bucket=PROJ, object_path="{}/{}/test_dict.pkl".format(EXPERIMENT, TAG)
+        )
+        self.assertEqual(expected_dict, pickle_content)
