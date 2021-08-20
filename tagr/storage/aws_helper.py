547001272d1ae061a0e4f88ccb7838acc8465dcd
sunriselong <sunrise.long@yahoo.ca>
2021-08-20 11:10:49 -0400

    lint outstanding files

--- tagr/storage/aws_helper.py
+++ tagr/storage/aws_helper.py
@@ -7,8 +7,9 @@ import json
 import pandas as pd
 import pickle
 
+
 class AwsHelper:
-    def get_matching_s3_objects(self, bucket, object_path='', max_keys_per_request=1):
+    def get_matching_s3_objects(self, bucket, object_path="", max_keys_per_request=1):
         """
         List objects in an S3 bucket.
 
@@ -19,17 +20,17 @@ class AwsHelper:
         max_keys_per_request: number of objects to list down
         """
 
-        s3 = boto3.client('s3')
-        kwargs = {'Bucket': bucket}
+        s3 = boto3.client("s3")
+        kwargs = {"Bucket": bucket}
 
         # If the prefix is a single string (not a tuple of strings), we can
         # do the filtering directly in the S3 API.
         if isinstance(object_path, str):
-            kwargs['Prefix'] = object_path
+            kwargs["Prefix"] = object_path
         else:
-            kwargs['Prefix'] = str(object_path)
+            kwargs["Prefix"] = str(object_path)
 
-        kwargs['MaxKeys'] = max_keys_per_request
+        kwargs["MaxKeys"] = max_keys_per_request
 
         while True:
 
@@ -38,12 +39,12 @@ class AwsHelper:
             resp = s3.list_objects_v2(**kwargs)
 
             try:
-                contents = resp['Contents']
+                contents = resp["Contents"]
             except KeyError:
                 return
 
             for obj in contents:
-                key = obj['Key']
+                key = obj["Key"]
                 if key.startswith(object_path):
                     yield obj
 
@@ -51,22 +52,26 @@ class AwsHelper:
             # Pass the continuation token into the next response, until we
             # reach the final page (when this field is missing).
             try:
-                kwargs['ContinuationToken'] = resp['NextContinuationToken']
+                kwargs["ContinuationToken"] = resp["NextContinuationToken"]
             except KeyError:
                 break
 
-    def get_matching_s3_keys(self, bucket, object_path='', max_keys_per_request=1):
+    def get_matching_s3_keys(self, bucket, object_path="", max_keys_per_request=1):
         """
         Generate the keys in an S3 bucket.
 
         Parameters
         ----------
         bucket: Name of the S3 bucket.
-        object_path: Path that leads to all tables 
+        object_path: Path that leads to all tables
         max_keys_per_request: number of objects to list down
         """
-        for obj in self.get_matching_s3_objects(bucket=bucket, object_path=object_path, max_keys_per_request=max_keys_per_request):
-            yield obj['Key']
+        for obj in self.get_matching_s3_objects(
+            bucket=bucket,
+            object_path=object_path,
+            max_keys_per_request=max_keys_per_request,
+        ):
+            yield obj["Key"]
 
     def get_list_of_tables(self, bucket, object_path):
         """
@@ -76,13 +81,15 @@ class AwsHelper:
         Parameters
         ----------
         bucket: name of s3 bucket
-        object_path: Path that leads to all tables 
+        object_path: Path that leads to all tables
         """
         s3_objects = []
 
         if object_path:
-            for full_object_path in self.get_matching_s3_keys(bucket=bucket, object_path=object_path, max_keys_per_request=1):
-                s3_objects.append(bucket + '/' + full_object_path)
+            for full_object_path in self.get_matching_s3_keys(
+                bucket=bucket, object_path=object_path, max_keys_per_request=1
+            ):
+                s3_objects.append(bucket + "/" + full_object_path)
 
         return s3_objects
 
@@ -95,15 +102,12 @@ class AwsHelper:
         bucket: name of s3 bucket (project)
         object_path: Path that leads to the object being fetched
         """
-        s3 = boto3.client('s3')
-        response = s3.get_object(
-            Bucket = bucket,
-            Key = object_path
-        )
+        s3 = boto3.client("s3")
+        response = s3.get_object(Bucket=bucket, Key=object_path)
 
-        if object_path.endswith('.csv'):
-            obj = pd.read_csv(response['Body'], index_col=0)
+        if object_path.endswith(".csv"):
+            obj = pd.read_csv(response["Body"], index_col=0)
         else:
-            serialized_model = response['Body'].read()
+            serialized_model = response["Body"].read()
             obj = pickle.loads(serialized_model)
         return obj
--- tagr/storage/local.py
+++ tagr/storage/local.py
@@ -6,8 +6,9 @@ import pandas as pd
 
 logger = logging.getLogger("saving experiment to local storage")
 
+
 class Local:
-    name = 'Local'
+    name = "Local"
 
     def dump_csv(self, df, proj, experiment, tag, filename):
         """
@@ -16,12 +17,14 @@ class Local:
         Parameters
         ----------
         df: dataframe object
-        proj: project name 
+        proj: project name
         experiment: experiment name
         tag: custom commit message
         filename: filename to be saved locally
         """
-        df.to_csv("{}/{}/{}/{}.csv".format(proj, experiment, tag, filename), index=False)
+        df.to_csv(
+            "{}/{}/{}/{}.csv".format(proj, experiment, tag, filename), index=False
+        )
 
     def dump_json(self, df_metadata, proj, experiment, tag):
         """
@@ -30,11 +33,13 @@ class Local:
         Parameters
         ----------
         df_metadata: json object containing experiment metadata
-        proj: project name 
+        proj: project name
         experiment: experiment name
         tag: custom commit message
         """
-        with open("{}/{}/{}/df_summary.json".format(proj, experiment, tag), 'w') as outfile:
+        with open(
+            "{}/{}/{}/df_summary.json".format(proj, experiment, tag), "w"
+        ) as outfile:
             json.dump(df_metadata, outfile, default=str)
 
     def dump_pickle(self, model, proj, experiment, tag, filename):
@@ -44,13 +49,15 @@ class Local:
         Parameters
         ----------
         pickle_object: model that has been serialized into a pickle object
-        proj: project name 
+        proj: project name
         experiment: experiment name
         tag: custom commit message
         filename: filename to be exported
         """
-        pickle.dump(model, open("{}/{}/{}/{}.pkl".format(proj, experiment, tag, filename), 'wb'))
-    
+        pickle.dump(
+            model, open("{}/{}/{}/{}.pkl".format(proj, experiment, tag, filename), "wb")
+        )
+
     def _Tagr__list(self, proj, experiment, tag):
         """
         gets list of files/folders located at {proj}/{experiment}/{tag}
@@ -63,8 +70,8 @@ class Local:
         """
         path = proj + "/" + experiment
         if tag:
-            path += ('/' + tag)
-    
+            path += "/" + tag
+
         folders = os.listdir(path)
         return folders
 
@@ -73,7 +80,7 @@ class Local:
         if path.endswith(".csv"):
             obj = pd.read_csv(path)
         else:
-            obj = pickle.load(open(path, 'rb'))
+            obj = pickle.load(open(path, "rb"))
         return obj
 
     def build_path(self, proj, experiment, tag):
@@ -82,11 +89,14 @@ class Local:
 
         Parameters
         ----------
-        proj: project name 
+        proj: project name
         experiment: experiment name
         tag: custom commit message
         """
         try:
             os.makedirs("{}/{}/{}".format(proj, experiment, tag))
         except OSError:
-            logger.info("The directory %s already exists. If using the tag argument, please provide a new unique identifier." % "{}/{}/{}".format(proj, experiment, tag))
+            logger.info(
+                "The directory %s already exists. If using the tag argument, please provide a new unique identifier."
+                % "{}/{}/{}".format(proj, experiment, tag)
+            )
