aa2eb15f326c076287fdc48243526b4369524aa7
SunriseLong <44146580+SunriseLong@users.noreply.github.com>
2021-08-20 11:16:16 -0400

    Add Black to CI (#26)
    * run black check
    
    * lint outstanding files

--- tagr/config.py
+++ tagr/config.py
@@ -1,7 +1,35 @@
-EXP_OBJECTS = ['X_train', 'X_test', 'y_train', 'y_test', 'model', 'prediction', 'y_pred']
+EXP_OBJECTS = [
+    "X_train",
+    "X_test",
+    "y_train",
+    "y_test",
+    "model",
+    "prediction",
+    "y_pred",
+]
 
-EXP_OBJECT_TYPES = ['dataframe', 'dataframe', 'dataframe', 'dataframe', 'model', 'dataframe', 'dataframe']
+EXP_OBJECT_TYPES = [
+    "dataframe",
+    "dataframe",
+    "dataframe",
+    "dataframe",
+    "model",
+    "dataframe",
+    "dataframe",
+]
 
-OBJECTS = {'X_train': 'dataframe', 'X_test': 'dataframe', 'y_train': 'dataframe', 'y_test': 'dataframe', 'model': 'model',
-           'trained_model': 'model', 'prediction': 'dataframe', 'y_pred': 'dataframe', 'accuracy': 'dataframe',
-           'int': 'primitive', 'float': 'primitive', 'str': 'primitive', 'bool': 'primitive'}
+OBJECTS = {
+    "X_train": "dataframe",
+    "X_test": "dataframe",
+    "y_train": "dataframe",
+    "y_test": "dataframe",
+    "model": "model",
+    "trained_model": "model",
+    "prediction": "dataframe",
+    "y_pred": "dataframe",
+    "accuracy": "dataframe",
+    "int": "primitive",
+    "float": "primitive",
+    "str": "primitive",
+    "bool": "primitive",
+}
